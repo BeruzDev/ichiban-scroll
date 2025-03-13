30e336523d693dbf7ee96228dfc42c1f5845cf9e
BeruzDev <albertus1291@gmail.com>
2025-03-13 19:38:02 +0100

    Scroll animation done

---
+++ frames.js
@@ -0,0 +1,59 @@
+import { images } from './images.js'
+
+// Frames maximos
+const MAX_FRAMES = 151
+
+// Elemento main
+const main = document.querySelector('main')
+
+// Frame actual
+let currentFrame = 0
+
+// Actualizar el src de la imagen con la propiedad .p de images.js
+function updateImage(frame = 0){
+	const src = images[frame].p
+	img.src = src
+}
+
+// Empezar con la primera imagen
+const img = document.createElement('img')
+
+// Append img a main
+main.appendChild(img)
+updateImage(currentFrame)
+
+// Altura maxima del scroll
+const maxScroll = document.documentElement.scrollHeight - window.innerHeight
+
+// Ajustar el altura maxima del scroll si cambia el tamaño de la pantalla
+window.addEventListener('resize', () => {
+	maxScroll = document.documentElement.scrollHeight - window.innerHeight
+})
+
+// Ultimo frame
+let lastFrameUpdate = 0
+
+// Evento scroll
+window.addEventListener('scroll', () => {
+	// Comprobar el ultimo frame
+	if(Date.now() - lastFrameUpdate < 1) return true
+
+	// Actualizamos el contador
+	lastFrameUpdate = Date.now()
+
+	// Posición actual de scroll
+	const scrollPosition = window.scrollY
+
+	// Calcular el porcentaje del scroll
+	const scrollFraction = scrollPosition / maxScroll
+
+	// Calculamos que frame toca ahora
+	const frame = Math.floor(scrollFraction * MAX_FRAMES)
+
+	// Si el frame que le toca es el mismo que ya tenia evitamos actualizar
+	if(currentFrame !== frame) {
+		// Creamos el id del nombre del archivo
+		updateImage(frame)
+		currentFrame = frame
+	}
+})
